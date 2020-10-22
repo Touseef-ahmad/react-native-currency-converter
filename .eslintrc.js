d2eff3094557da463c0f95acb7f02905bdcd277c
Touseef Ahmad <touseef.ahmad.cs@gmail.com>
2020-10-22 11:58:49 +0500

    :bug: fix eslint issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,10 +9,9 @@ module.exports = {
     'no-mixed-operators': 'off', // Incompatible with prettier
     'arrow-body-style': 'off', // Not our taste?
     'function-paren-newline': 'off', // Incompatible with prettier
-    'global-require': 'off',
+
     'no-plusplus': 'off',
     'space-before-function-paren': 0, // Incompatible with prettier
-
     'max-len': ['error', 100, 2, { ignoreUrls: true }], // airbnb is allowing some edge cases
     'no-console': 'error', // airbnb is using warn
     'no-alert': 'error', // airbnb is using warn
--- src/screens/currency-list/flat-list-item/flat-list-item.js
+++ src/screens/currency-list/flat-list-item/flat-list-item.js
@@ -1,37 +1,38 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { Text } from 'react-native';
 
 import { RowItem } from '../../../components/row-item';
 
-import { propTypes } from './prop-types';
-
-import { ConversionContext } from '../../../utils';
-
-export const FlatListItem = ({ item, isBaseCurrency, navigation }) => {
-  const { baseCurrency, quoteCurrency, setBaseCurrency, setQuoteCurrency } = useContext(
-    ConversionContext,
-  );
-
+export const FlatListItem = (
+  item,
+  navigation,
+  isBaseCurrency,
+  baseCurrency,
+  quoteCurrency,
+  setBaseCurrency,
+  setQuoteCurrency,
+) => {
   let selected = false;
-
-  if (isBaseCurrency && item === baseCurrency) {
+  if (isBaseCurrency && item.item === baseCurrency) {
     selected = true;
-  } else if (!isBaseCurrency && item === quoteCurrency) {
+  } else if (!isBaseCurrency && item.item === quoteCurrency) {
     selected = true;
   }
 
   const navigateBack = () => {
     if (isBaseCurrency) {
-      setBaseCurrency(item);
+      setBaseCurrency(item.item);
     } else {
-      setQuoteCurrency(item);
+      setQuoteCurrency(item.item);
     }
     navigation.pop();
   };
 
   return (
-    <RowItem text={item} onPress={navigateBack} rightIcon={selected && <Text>&#10004;</Text>} />
+    <RowItem
+      text={item.item}
+      onPress={navigateBack}
+      rightIcon={selected && <Text>&#10004;</Text>}
+    />
   );
 };
-
-FlatListItem.propTypes = propTypes;
